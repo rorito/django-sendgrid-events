9cf614af491adcf5875dd8ae321452b21b6f124b
Abel Santos <adsantos07@gmail.com>
2015-06-11 18:15:04 +0800

    #1006 - Support Celery Tasks
    - updated to use process the sendgrid events on celery if the settings variable
      SENDGRID_BACKGROUND_PROCESSING is set to True

--- sendgrid_events/views.py
+++ sendgrid_events/views.py
@@ -1,12 +1,31 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from sendgrid_events.models import Event
+from sendgrid_events.models import Event, process_batch
 
 
 @require_POST
 @csrf_exempt
 def handle_batch_post(request):
-    Event.process_batch(data=request.body)
+    background_process = False
+    if hasattr(settings, 'SENDGRID_BACKGROUND_PROCESSING'):
+        background_process = settings.SENDGRID_BACKGROUND_PROCESSING
+
+    queue = None
+    if hasattr(settings, 'SENDGRID_BACKGROUND_QUEUE'):
+        queue = settings.SENDGRID_BACKGROUND_QUEUE
+
+    if background_process:
+        if queue:
+            process_batch.apply_async(
+                kwargs={'data': request.body},
+                queue=queue
+            )
+        else:
+            process_batch.apply_async(kwargs={'data': request.body})
+
+    else:
+        Event.process_batch(data=request.body)
     return HttpResponse()
